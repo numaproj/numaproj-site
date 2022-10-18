3132c5056b7de8d8dd875e7e2ec2cb48a169254f
YuechenL <49246577+YuechenL@users.noreply.github.com>
2022-10-18 09:53:15 -0700

    add footer (#5)
    Signed-off-by: Yuechen Liu <yuechen_liu@intuit.com>
    Co-authored-by: Yuechen Liu <yuechen_liu@intuit.com>

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+    return (
+        <>
+            <div className="text-center p-4 py-16 text-[10px]">2022 © Numaproj Authors. All rights reserved.</div>
+        </>
+    )
+}
+
+export default Footer
