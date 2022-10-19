612f7d0ffe4e9ab92eb662b7c888d1f1493a30f2
Dan Smith <dan_smith@intuit.com>
2022-10-18 19:42:52 -0700

    updating button font

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ export interface ButtonProps  {
 
 export default function Button({ href, text, target="_self" }: ButtonProps) {
     return <>
-        <a href={href} target={target} rel={target=="_blank" ? "noopener noreferrer":""} className="inline-block text-white px-9 py-4 bg-blue hover:bg-blue/50 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+        <a href={href} target={target} rel={target=="_blank" ? "noopener noreferrer":""} className="inline-block text-white px-9 py-4 bg-blue hover:bg-blue/50 text-white font-medium text-sm leading-tight rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
             {text}
         </a>
     </>
